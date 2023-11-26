a0e926a95868380ba5848bb8f31db4a79e847f46
Bimbo Ogungbe <bimbo@MacBook-Pro-2.local>
2023-11-26 17:48:47 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+}
+
+
+
+
+
+
+
+provider "aws" {
+  region     = "eu-west-1"
+}
+
+
+resource "aws_instance" "bimbo_instance" {
+  ami           = "ami-07355fe79b493752d"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "testInstance"
+  }
+}
+
